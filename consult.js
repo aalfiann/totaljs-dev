0eac21c39f77c8d3590747a81a59956701825de5
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-07-06 17:35:35 +0700

    - Update consult socket - Add wsconsult module

--- consult.js
+++ consult.js
@@ -16,97 +16,20 @@ const options = {};
 //require('total.js/debug')(options);
 
 require('total.js')
+
+//======================================
+// BELOW HERE IS TO RUN SOCKET SERVER   
+//======================================
 const uuidv4 = require('uuid/v4');
 var socketio = require('socket.io');
 var revalidator = require('revalidator');
+var wsconsult = require(F.path.definitions('wsconsult'));
 
 F.on("load", function() {
     this.io = socketio.listen(this.server);
 
     this.io.on('connection',function(socket){
-
-        function BalikanHeader (stsres, stsdes, stsfal, datanya) {
-            var teksnya;
-            if (datanya == '') {
-                teksnya = '{"sts_res":"' + stsres + '","sts_des":"' + stsdes + '","sts_fal":"' + stsfal + '"}';
-            } else {
-                teksnya = '{"sts_res":"' + stsres + '","sts_des":"' + stsdes + '","sts_fal":"' + stsfal + '", "data":[' + datanya + ']}';
-            }
-            return teksnya;
-        }
-
-        function validateConsult(transaksi_konsul_id){
-            try {
-                var sql = NOSQL('tr_transaksi_konsul');
-                var tanggal = new Date().toISOString().replace("T", " ").replace("Z", "").substr(0,19);
-                sql.find().make(function(builder) {
-                    builder.where('transaksi_konsul_id', transaksi_konsul_id);
-                    builder.where('tanggal_awal_chat','<=',tanggal);
-                    builder.where('tanggal_akhir_chat','>=',tanggal);
-                    builder.where('status_active_id',1);
-                    builder.callback(function(err, response,count) {
-                        if(count){
-                            socket.emit('validateConsult',JSON.parse(BalikanHeader('true','Consult masih aktif','',JSON.stringify(response))));
-                        } else {
-                            socket.emit('validateConsult',JSON.parse(BalikanHeader('true','Consult telah expired','expired',JSON.stringify(response))));
-                            socket.disconnect();
-                        }
-                    });
-                });
-            } catch (err) {
-                socket.emit('validateConsult',JSON.parse(BalikanHeader("false","Ada kesalahan... " + err,"error","")));
-                socket.disconnect();
-            }
-        }
-
-        function loadMessages(transaksi_konsul_id){
-            try {
-                var sql = NOSQL('tr_chat_messages');
-                sql.find().make(function(builder) {
-                    builder.where('transaksi_konsul_id', transaksi_konsul_id);
-                    builder.callback(function(err, response, count) {
-                        if(count>0){
-                            socket.emit('loadHistory',JSON.parse(BalikanHeader('true','History chat ditemukan','',JSON.stringify(response))));
-                        } else {
-                            socket.emit('loadHistory',JSON.parse(BalikanHeader('true','History chat tidak ditemukan','',JSON.stringify(response))));
-                        }
-                    });
-                });
-            } catch (err) {
-                socket.emit('loadHistory',JSON.parse(BalikanHeader("false","Ada kesalahan... " + err,"error","")));
-            }
-        }
-
-        function insertMessages(data){
-            try {
-                var sql = NOSQL('tr_chat_messages');
-                sql.insert(data);
-                return BalikanHeader('true','Pesan masuk','',JSON.stringify(data));
-            } catch (err) {
-                return BalikanHeader("false","Ada kesalahan... " + err,"error","");
-            }
-        }
-
-        function messageIsRead(data){
-            try {
-                var sql = NOSQL('tr_chat_messages');
-                sql.update({ messages_status_id: 3}).make(function(builder) {
-                    builder.first();
-                    builder.where('messages_id', data.messages_id);
-                    builder.where('messages_status_id', '!=',3);
-                    builder.callback(function(err,response,count) {
-                        socket.broadcast.to(data.transaksi_konsul_id).emit('read', JSON.parse(BalikanHeader('true','Status pesan telah read','',JSON.stringify(data))));
-                    });
-                });
-            } catch (err) {
-                socket.broadcast.to(data.transaksi_konsul_id).emit('read', JSON.parse(BalikanHeader("false","Ada kesalahan... " + err,"error","")));
-            }
-        }
-
-        function messageIsTyping(data){
-            socket.broadcast.to(data.transaksi_konsul_id).emit('typed', JSON.parse(BalikanHeader('true','Sedang mengetik...','',JSON.stringify(data))));
-        }
-
+        
         console.log('Client id: '+socket.id+' is connected');
 
         socket.on('join', function(data) {
@@ -118,40 +41,12 @@ F.on("load", function() {
                 isi_messages:'is join to this chat.'
             });
             // send emit to load event with json data message
-            loadMessages(data.transaksi_konsul_id);
+            wsconsult.loadMessages(data.transaksi_konsul_id,socket);
             // validate consult if expired then disconnect
-            validateConsult(data.transaksi_konsul_id);
+            wsconsult.validateConsult(data.transaksi_konsul_id,socket);
         });
 
         socket.on('message', function(data) {
-
-            var message_schema = {
-                type: 'object',
-                required: true,
-                properties: {
-                    transaksi_konsul_id: {
-                        type: 'string',
-                        required: true
-                    },
-                    akun_id: {
-                        type: 'string',
-                        required: true
-                    },
-                    akun_type: {
-                        type: 'number',
-                        required: true
-                    },
-                    message_type_id: {
-                        type: 'number',
-                        required: true
-                    },
-                    isi_messages: {
-                        type: 'string',
-                        required: true
-                    }
-                }
-            };
-
             var sendData = {
                 transaksi_konsul_id:data.transaksi_konsul_id,
                 akun_id:data.akun_id,
@@ -160,28 +55,26 @@ F.on("load", function() {
                 isi_messages:data.isi_messages
             };
 
-            var validator = revalidator.validate(sendData,message_schema);
+            var validator = revalidator.validate(sendData,wsconsult.message_schema);
             if(validator.valid){
                 sendData.messages_id = uuidv4();
                 sendData.messages_date = new Date().toISOString().replace("T", " ").replace("Z", "").substr(0,19);
                 sendData.messages_status_id=1;
                 // write logic to saving message to database with message status received in database
-                var resData = insertMessages(sendData);
+                var resData = wsconsult.insertMessages(sendData);
                 console.log('sending ', sendData);
                 socket.broadcast.to(data.transaksi_konsul_id).emit('received', JSON.parse(resData));
             } else {
-                socket.emit('received',JSON.parse(BalikanHeader("false","Ada kesalahan... "+ JSON.stringify(JSON.stringify(validator.errors)).substr(1).slice(0, -1),"error","")));
+                socket.emit('received',JSON.parse(wsconsult.BalikanHeader("false","Ada kesalahan... "+ JSON.stringify(JSON.stringify(validator.errors)).substr(1).slice(0, -1),"error","")));
             }
         });
 
         socket.on('reading', function(data) {
-            // write logic to set message status to read in database
-            messageIsRead(data);
+            wsconsult.messageIsRead(data,socket);
         });
 
         socket.on('typing', function(data) {
-            // write logic to send message status is typing
-            messageIsTyping(data);
+            wsconsult.messageIsTyping(data,socket);
         });
 
         socket.on('disconnect', function (data) {
---
+++ definitions/wsconsult.js
@@ -0,0 +1,152 @@
+/**
+ * Module for WS Consult Chat
+ */
+
+ // Require helper module
+const helper = require(F.path.definitions('helper'));
+
+/**
+ * Schema Message for validate the socket request 
+ * @return {object}
+ */
+function message_schema(){
+    return {
+        type: 'object',
+        required: true,
+        properties: {
+            transaksi_konsul_id: {
+                type: 'string',
+                required: true
+            },
+            akun_id: {
+                type: 'string',
+                required: true
+            },
+            akun_type: {
+                type: 'number',
+                required: true
+            },
+            message_type_id: {
+                type: 'number',
+                required: true
+            },
+            isi_messages: {
+                type: 'string',
+                required: true
+            }
+        }
+    };
+}
+
+/**
+ * Validate the consult id
+ * @param {string} transaksi_konsul_id 
+ * @param {*} socket 
+ * @return {callback} emit to validateConsult event or socket disconnect
+ */
+function validateConsult(transaksi_konsul_id,socket){
+    try {
+        var sql = NOSQL('tr_transaksi_konsul');
+        var tanggal = new Date().toISOString().replace("T", " ").replace("Z", "").substr(0,19);
+        sql.find().make(function(builder) {
+            builder.where('transaksi_konsul_id', transaksi_konsul_id);
+            builder.where('tanggal_awal_chat','<=',tanggal);
+            builder.where('tanggal_akhir_chat','>=',tanggal);
+            builder.where('status_active_id',1);
+            builder.callback(function(err, response,count) {
+                if(count){
+                    socket.emit('validateConsult',JSON.parse(helper.BalikanHeader('true','Consult masih aktif','',JSON.stringify(response))));
+                } else {
+                    socket.emit('validateConsult',JSON.parse(helper.BalikanHeader('true','Consult telah expired','expired',JSON.stringify(response))));
+                    socket.disconnect();
+                }
+            });
+        });
+    } catch (err) {
+        socket.emit('validateConsult',JSON.parse(helper.BalikanHeader("false","Ada kesalahan... " + err,"error","")));
+        socket.disconnect();
+    }
+}
+
+/**
+ * Load History Messages
+ * @param {string} transaksi_konsul_id 
+ * @param {*} socket 
+ * @return {callback} emit to loadHistory event
+ */
+function loadMessages(transaksi_konsul_id,socket){
+    try {
+        var sql = NOSQL('tr_chat_messages');
+        sql.find().make(function(builder) {
+            builder.where('transaksi_konsul_id', transaksi_konsul_id);
+            builder.callback(function(err, response, count) {
+                if(count>0){
+                    socket.emit('loadHistory',JSON.parse(helper.BalikanHeader('true','History chat ditemukan','',JSON.stringify(response))));
+                } else {
+                    socket.emit('loadHistory',JSON.parse(helper.BalikanHeader('true','History chat tidak ditemukan','',JSON.stringify(response))));
+                }
+            });
+        });
+    } catch (err) {
+        socket.emit('loadHistory',JSON.parse(helper.BalikanHeader("false","Ada kesalahan... " + err,"error","")));
+    }
+}
+
+/**
+ * Insert Messages to database
+ * @param {*} data 
+ * @return {string} 
+ */
+function insertMessages(data){
+    try {
+        var sql = NOSQL('tr_chat_messages');
+        sql.insert(data);
+        return helper.BalikanHeader('true','Pesan masuk','',JSON.stringify(data));
+    } catch (err) {
+        return helper.BalikanHeader("false","Ada kesalahan... " + err,"error","");
+    }
+}
+
+/**
+ * Broadcast event message is typing to all client in same room
+ * @param {*} data 
+ * @param {*} socket 
+ * @return {callback} emit to typed event
+ */
+function messageIsTyping(data,socket){
+    socket.broadcast.to(data.transaksi_konsul_id).emit('typed', JSON.parse(helper.BalikanHeader('true','Sedang mengetik...','',JSON.stringify(data))));
+}
+
+/**
+ * Broadcast event message is read to all client in same room
+ * @param {*} data 
+ * @param {*} socket 
+ * @return {callback} emit to read event
+ */
+function messageIsRead(data,socket){
+    try {
+        var sql = NOSQL('tr_chat_messages');
+        sql.update({ messages_status_id: 3}).make(function(builder) {
+            builder.first();
+            builder.where('messages_id', data.messages_id);
+            builder.where('messages_status_id', '!=',3);
+            builder.callback(function(err,response,count) {
+                socket.broadcast.to(data.transaksi_konsul_id).emit('read', JSON.parse(helper.BalikanHeader('true','Status pesan telah read','',JSON.stringify(data))));
+            });
+        });
+    } catch (err) {
+        socket.broadcast.to(data.transaksi_konsul_id).emit('read', JSON.parse(helper.BalikanHeader("false","Ada kesalahan... " + err,"error","")));
+    }
+}
+
+module.exports = {
+    BalikanHeader : function(stsres, stsdes, stsfal, datanya){
+        return helper.BalikanHeader(stsres, stsdes, stsfal, datanya);
+    },
+    validateConsult,
+    loadMessages,
+    insertMessages,
+    messageIsTyping,
+    messageIsRead,
+    message_schema
+}
