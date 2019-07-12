1cbd1091e380e3f7c2ef52376d9bfef78b0d4e04
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-07-12 20:00:27 +0700

    - update test

--- test.js
+++ test.js
@@ -17,20 +17,36 @@ const options = {};
 
 require('total.js')
 
-var helper = require(F.path.definitions('helper'));
-//var to = ["f1AXw48goHw:APA91bEcjBDKFQw6goEkAwGB5DtoMfCAEbIEuRE-wL7Ce6GtqXHuqGqAAaNScVMKkASHvCzdkW5P3ykHcfaDxbsBNH0nL0HHGbTKigi0IoD3X7HOor7CiemsZGi8yXD2vDRiboaQJPpz","abcbc"];
-var to = ["abcbc"];
-var data = {
-          "body" : "Body of Your Notification in Data",
-          "title": "Title of Your Notification in Title",
-          "key_1" : "Value for key_1",
-          "key_2" : "Value for key_2"
-}
+// var helper = require(F.path.definitions('helper'));
+// //var to = ["f1AXw48goHw:APA91bEcjBDKFQw6goEkAwGB5DtoMfCAEbIEuRE-wL7Ce6GtqXHuqGqAAaNScVMKkASHvCzdkW5P3ykHcfaDxbsBNH0nL0HHGbTKigi0IoD3X7HOor7CiemsZGi8yXD2vDRiboaQJPpz","abcbc"];
+// var to = ["cGA2OmCCsGs:APA91bGN33kohGlb8LalePk3Ruw8gW3hXcoStJvGImXYBrU19kJZ9ZIumBvb_oSp-QIcPe-F7XN5OMUQ6bTUo8HXx5FJO74iXr6yCCWkDlCexN1SSkEN8agBEj6S31wZ_xIwhmsIPnTn"];
+// var data = {
+//           "body" : "Body of Your Notification in Data",
+//           "title": "Title of Your Notification in Title",
+//           "key_1" : "Value for key_1",
+//           "key_2" : "Value for key_2"
+// }
 
-helper.sendFCM(to,data,'',function(response){
-    console.log(JSON.stringify(response));
-});
+// helper.sendFCM(to,data,'',function(response){
+//     console.log(JSON.stringify(response));
+// });
 
 // console.log(helper.testFCM(to,data));
 
+F.on("load", function() {
+    var users = NOSQL('users');
+    var search = 'admin';
+
+	// Reads the profile
+	users.find()
+		.make(function(builder) {
+			var profile = builder.join('id', NOSQL('profile')).on('id', 'id');
+            builder.contains('id');
+            profile.first();
+            profile.search('about', search);
+            builder.callback(function (err,response,count){
+				console.log(JSON.stringify(response));
+			});
+		});
+});
 F.http('debug');
