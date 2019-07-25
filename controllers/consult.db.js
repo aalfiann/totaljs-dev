8e3f5af2ffb33954546c254aacde3bfdea1afe15
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2019-07-25 15:56:13 +0700

    Add get model.contoh into javascript in view

--- controllers/consult.db.js
+++ controllers/consult.db.js
@@ -13,6 +13,11 @@ function view_consult(akun_type,akun_id) {
 
 function view_consultroom(akun_type,akun_id,transaksi_konsul_id) {
     var self = this;
-	self.view('consult-db',{ consultroom: transaksi_konsul_id,usertype:akun_type,userid: akun_id });
+	self.view('consult-db',{ 
+        consultroom: transaksi_konsul_id,
+        usertype:akun_type,
+        userid: akun_id,
+        contoh: JSON.stringify([[{name:"aziz"}]])
+    });
 
 }
